cf35713de903e008746b13f6c75cc619e5993b92
Daniel Bronsky <dragonvedb@gmail.com>
2022-12-23 13:55:42 +0300

    Create a general operate function

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '\/':
+            return divide(a, b);
+    }
+}
